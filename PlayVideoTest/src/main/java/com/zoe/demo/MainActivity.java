f7bcec4fde17bedc8ddb18872d94b576a982ac54
ZoeWangW <13438165026@163.com>
2018-10-09 20:49:13 +0800

    播放视频项目，权限问题没有解决，闪退
    Signed-off-by: ZoeWangW <13438165026@163.com>

--- PlayVideoTest/src/main/java/com/zoe/demo/MainActivity.java
+++ PlayVideoTest/src/main/java/com/zoe/demo/MainActivity.java
@@ -1,52 +1,69 @@
 package com.zoe.demo;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Environment;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.VideoView;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    private Button play;
+
+    private Button pause;
+
+    private Button replay;
+
+    private VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        play = (Button) findViewById(R.id.play);
+        pause = (Button) findViewById(R.id.pause);
+        replay = (Button) findViewById(R.id.replay);
+        play.setOnClickListener(this);
+        pause.setOnClickListener(this);
+        replay.setOnClickListener(this);
+        initVideoPath();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    }
+
+    private void initVideoPath(){
+            File file = new File(Environment.getExternalStorageDirectory(),"movie.mp3");
+            videoView.setVideoPath(file.getPath());
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.play:
+                if (!videoView.isPlaying()){
+                    videoView.start();
+                }
+                break;
+            case R.id.pause:
+                if (videoView.isPlaying()){
+                    videoView.pause();
+                }
+                break;
+            case R.id.replay:
+                if (videoView.isPlaying()){
+                    videoView.resume();
+                }
+                break;
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        if (videoView != null){
+            videoView.suspend();
+        }
     }
 }
